a75a73325921c38672923cf4c6347f8b69c5132b
Tim Hårek Andreassen <tim@harek.no>
2023-02-01 21:23:36 +0100

    chore: Update details
    Signed-off-by: Tim Hårek Andreassen <tim@harek.no>

--- README.md
+++ README.md
@@ -1,12 +1,5 @@
-# cliffy-starter
+# deno-linkding
 
-Good defaults for CLIs built with [Deno][deno] and [Cliffy][cliffy].
+Access your Linkding instance with Deno.
 
-## Usage
-
-```sh
-git clone https://git.sr.ht/~timharek/cliffy-starter
-```
-
-[deno]: https://deno.land
-[cliffy]: https://cliffy.io
+Currently a work in progress.
--- deps.ts
+++ deps.ts
@@ -1,9 +1,9 @@
 export { Command } from 'https://deno.land/x/cliffy@v0.25.7/command/mod.ts';
 
 export const Config = {
-  name: 'cliffy-starter',
+  name: 'linkding',
   version: 'v1.0.0',
-  description: 'Good defaults for CLIs built with Cliffy and Deno.',
-  author: 'First Lastname <mail@example.org>',
-  source: 'https://example.org/repository',
+  description: 'Access your Linkding instance.',
+  author: 'Tim Hårek Andreassen <tim@harek.no>',
+  source: 'https://deno.land/x/linkding',
 };
