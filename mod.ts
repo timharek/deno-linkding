d5ed94cc253628287c849534d5fffe431feea8c1
Tim Hårek Andreassen <tim@harek.no>
2023-02-05 00:23:29 +0100

    feat: Add module
    Signed-off-by: Tim Hårek Andreassen <tim@harek.no>

---
+++ mod.ts
@@ -0,0 +1,24 @@
+/**
+ * Simple module for accessing your [Linkding](https://github.com/sissbruecker/linkding) instance.
+ *
+ * ## Example for getting all bookmarks
+ * ```ts
+ * import { listBookmarks } from 'https://deno.land/x/linkding/mod.ts';
+ *
+ * const allBookmarks = listBookmarks({ all: true });
+ * // do what you need to do with the bookmarks.
+ * ```
+ *
+ * ## Example for single bookmark
+ * ```ts
+ * import { getBookmark } from 'https://deno.land/x/linkding/mod.ts';
+ *
+ * const bookmark = getBookmark(10);
+ * // do what you need to do with the bookmark.
+ * ```
+ *
+ * @module
+ */
+
+export * from './src/read.ts';
+export * from './src/util.ts';
